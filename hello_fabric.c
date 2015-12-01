00aab04cd29bd4ce7b0481f1c0d2897e31029d72
Rocky Craig <rocky.craig@hpe.com>
2015-11-30 23:10:39 -0700

    add copyright notice

--- hello_fabric.c
+++ hello_fabric.c
@@ -1,3 +1,18 @@
+// Copyright 2015 Hewlett Packard Enterprise Development
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 // hello_fabric.c - First touch of Fabric-Attached Memory for The Machine
 // from Hewlett Packard Enterprise.  Compile this on any of the VMs created
 // by the configurator script.
