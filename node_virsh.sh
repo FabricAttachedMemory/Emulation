4adee03dcfd8a5a5a98ab09736f4b854adbca782
austin hunting <austin.hunting@hpe.com>
2017-06-19 14:17:10 -0600

    node_virsh.sh now searches execution directory for node xml files

--- node_virsh.sh
+++ node_virsh.sh
@@ -6,7 +6,8 @@ DIRECTIVE=${1-}
 
 set -u
 
-NODESXML=`ls node*.xml`
+CURRENT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NODESXML=`find $CURRENT_DIRECTORY/ -name "node*.xml" -printf "%f\n"`
 NODESDOM=`echo $NODESXML | sed 's/\.xml//g'`
 
 [ `id -u` = 0 ] && VIRSH="virsh" || VIRSH='sudo virsh'
