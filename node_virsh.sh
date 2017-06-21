9c5ac27b9a96884b409ae9075891798edd1c5ffc
austin hunting <austin.hunting@hpe.com>
2017-06-21 11:36:05 -0600

    Revert "Added cd to working EXECUTION_DIRECTORY" Revert to undo format changes This reverts commit 0bda05d4ca764f8e17cc5231bcd03ecd31c4a930.

--- node_virsh.sh
+++ node_virsh.sh
@@ -6,8 +6,8 @@ DIRECTIVE=${1-}
 
 set -u
 
-EXECUTION_DIRECTORY="$( dirname "${BASH_SOURCE[0]}" )" && cd $EXECUTION_DIRECTORY
-NODESXML=`find ./ -name "node*.xml" -printf "%f\n"`
+EXECUTION_DIRECTORY="$( dirname "${BASH_SOURCE[0]}" )"
+NODESXML=`find $EXECUTION_DIRECTORY/ -name "node*.xml" -printf "%f\n"`
 NODESDOM=`echo $NODESXML | sed 's/\.xml//g'`
 
 [ `id -u` = 0 ] && VIRSH="virsh" || VIRSH='sudo virsh'
@@ -15,37 +15,37 @@ NODESDOM=`echo $NODESXML | sed 's/\.xml//g'`
 case "$DIRECTIVE" in
 
 define)
-    for XML in $NODESXML; do $VIRSH define $XML; done
-    exit 0
-    ;;
+	for XML in $NODESXML; do $VIRSH define $EXECUTION_DIRECTORY"/"$XML; done
+	exit 0
+	;;
 
 destroy)
-    for DOM in $NODESDOM; do $VIRSH destroy $DOM; done
-    exit 0
-    ;;
+	for DOM in $NODESDOM; do $VIRSH destroy $DOM; done
+	exit 0
+	;;
 
 start)
-    for DOM in $NODESDOM; do $VIRSH start $DOM; done
-    exit 0
-    ;;
+	for DOM in $NODESDOM; do $VIRSH start $DOM; done
+	exit 0
+	;;
 
 status)
-    $VIRSH list --all | egrep -- 'Name|----|node'
-    exit 0
-    ;;
+	$VIRSH list --all | egrep -- 'Name|----|node'
+	exit 0
+	;;
 
 stop|shutdown)
-    for NODE in $NODESDOM; do
-        echo -n "$NODE: "
-        ssh $NODE sudo shutdown -h 0
-    done
-    exit 0
-    ;;
+	for NODE in $NODESDOM; do
+		echo -n "$NODE: "
+		ssh $NODE sudo shutdown -h 0
+	done
+	exit 0
+	;;
 
 undefine)
-    for DOM in $NODESDOM; do $VIRSH undefine $DOM; done
-    exit 0
-    ;;
+	for DOM in $NODESDOM; do $VIRSH undefine $DOM; done
+	exit 0
+	;;
 
 esac
 
